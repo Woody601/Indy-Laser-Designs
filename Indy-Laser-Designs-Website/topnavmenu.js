d0db2c232d7b694883e3ba53d3bab9d3fb110b96
Woody601 <mychalwood@gmail.com>
2022-05-17 23:33:00 -0400

    Add files via upload

---
+++ Indy-Laser-Designs-Website/topnavmenu.js
@@ -0,0 +1,6 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction(x) {
+  x.classList.toggle("change");
+  var x = document.getElementById("myTopnav");
+  x.classList.toggle("responsive");
+}
